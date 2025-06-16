b16efd61b1c7bd68128e1cb7af8687c17bb7502a
hyu758 <huy20041512@gmail.com>
2025-06-16 15:00:03 +0700

    fix bootstrap

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import App from './App.vue'
 import router from './router'
 import ToastPlugin from 'vue-toast-notification';
 import 'vue-toast-notification/dist/theme-sugar.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 const app = createApp(App)
 app.use(router)
 app.use(ToastPlugin)
